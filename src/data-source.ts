e92e4274ed13676da922902b82c38f4634a60d0b
Gustavo Castanho Duarte <gustavo@ACT-11490-EXT.local>
2025-04-02 22:08:13 -0300

    update database

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,14 +3,11 @@ import { DataSource } from "typeorm";
 import { Depoimentos } from "./entity/Depoimentos";
 import { EnderecosPontosContatos } from "./entity/EnderecosPontosContato";
 import { LogosPatrocinadores } from "./entity/LogosPratrocinadores";
+import "dotenv/config";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "heffalump.db.elephantsql.com",
-  port: 5432,
-  username: "tjnueevx",
-  password: "aItcxKH2jHy4hUTYgKl-hYb4IQ5gU8B4",
-  database: "tjnueevx",
+  url: String(process.env.DATABASE_URL),
   synchronize: true,
   logging: false,
   entities: [Depoimentos, EnderecosPontosContatos, LogosPatrocinadores],
--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,18 @@ import "reflect-metadata";
 import { AppDataSource } from "./data-source";
 import routes from "./routes";
 
-AppDataSource.initialize().then(async () => {
-  const app = express();
-  app.use(cors());
-  app.use(bodyParser.json());
-  app.use(routes);
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Conexão com o banco de dados estabelecida com sucesso!");
+    const app = express();
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(routes);
 
-  app.listen(3334);
-});
+    app.listen(3334, () => {
+      console.log("Servidor rodando na porta 3334");
+    });
+  })
+  .catch((error) => {
+    console.error("Erro ao conectar ao banco de dados:", error);
+  });
